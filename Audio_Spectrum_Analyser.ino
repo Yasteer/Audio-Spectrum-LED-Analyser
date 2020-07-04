019d1f8d534c78af6e041f87cb4aed299925d7a5
Yasteer Sewpersad <30473531+Yasteer@users.noreply.github.com>
2020-07-04 20:43:19 +0200

    Implemented FFT through an external lib
    Not sure if the ADC settings are correct for the sampling rate used in the FFT lib. Check for future commit.

--- Audio_Spectrum_Analyser.ino
+++ Audio_Spectrum_Analyser.ino
@@ -7,11 +7,17 @@
  * 1x Arduino Uno
  */
 
+#include <arduinoFFT.h>
+static const uint16_t samples = 128;
+static const double samplingFrequency = 1000; 
+double vReal[samples]; // Arrays to store values from the FFT evaluation. 
+double vImag[samples];
+
 int SS_Analogue_Reading = 0;
 bool SS_Digital_Reading = 0;
  
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   // ADC Setup
   DDRD = DDRD | B00000000; // Digital Input on pin PD2 
   ADMUX =  (0 << REFS1) | (1 << REFS0); // Use 5V reference, left adjust ADCH register,  and use default Channel 0 for input
@@ -20,18 +26,34 @@ void setup() {
 }
 
 void loop() {
-
+  // Check that sound sensor is functional.
   if(PIND & B00000100){ // Check if Pin PD2 is set. 
     SS_Digital_Reading = true;
   }
+
+  // Sample Signal
+  for(int count = 0; count < samples; count++){
+    if(SS_Digital_Reading == true){ // Only record an analogue reading if there is a suitable level of audio available. 
+      SS_Analogue_Reading = Read_ADC();
+      vReal[count] = SS_Analogue_Reading; 
+      vImag[count] = 0; // A real signal is being measured so there is no imaginary component.
+    }
+  }
   
-  if(SS_Digital_Reading == true){ // Only record an analogue reading if there is a suitable level of audio available. 
-    SS_Analogue_Reading = Read_ADC();
+  // Compute the Fast Fourier Transform --> Decomposes a signal into its frequencies.
+  // Follow this link to check function implementations: https://github.com/kosme/arduinoFFT/blob/master/src/arduinoFFT.cpp
+  arduinoFFT FFT = arduinoFFT(vReal, vImag, samples, samplingFrequency); // Intialize an FFT object from the library. Each object contains new data to sample.
+  FFT.Windowing(FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+  FFT.Compute(FFT_FORWARD);
+  FFT.ComplexToMagnitude();
+  FFT.MajorPeak();
+  
+  // Display Results
+  for(int disp1 = 0; disp1 < (samples/2); disp1++){
+    Serial.println(vReal[disp1],1);
   }
 
-  // Compute the Fast Fourier Transform --> Decomposes a signal into its frequencies.
-  
-
+  delay(1000);
 }
 
 unsigned int Read_ADC(){
